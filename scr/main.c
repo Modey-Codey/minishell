e9a150ed0fd415b66c4f9b497e323af369081997
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-05-12 20:20:18 +0700

    parser done

--- scr/main.c
+++ scr/main.c
@@ -1,16 +1,69 @@
 #include "minishell.h"
 
+static void	print_cmds(t_cmd *cmds)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	tmp = cmds;
+	while (tmp)
+	{
+		printf("CMD:\n");
+		i = 0;
+		while (tmp->argv && tmp->argv[i])
+		{
+			printf("argv[%d]: %s\n", i, tmp->argv[i]);
+			i++;
+		}
+		if (tmp->infile)
+			printf("infile: %s\n", tmp->infile);
+		if (tmp->outfile)
+			printf("outfile: %s\n", tmp->outfile);
+		if (tmp->append)
+			printf("append mode\n");
+		if (tmp->heredoc)
+			printf("heredoc: %s\n", tmp->delimiter);
+		tmp = tmp->next;
+	}
+}
+
+static void	process_input(char *input, t_shell *shell)
+{
+	t_token	*tokens;
+	t_cmd	*cmds;
+
+	if (has_unclosed_quote(input))
+	{
+		printf("unclosed quote\n");
+		return ;
+	}
+	tokens = tokenize(input);
+	if (!syntax_check(tokens))
+	{
+		free_tokens(tokens);
+		return ;
+	}
+	cmds = parse(tokens, shell);
+	print_cmds(cmds);
+	free_tokens(tokens);
+	free_cmds(cmds);
+}
+
 int	main(void)
 {
 	char	*input;
-	char	*expanded;
 	t_shell	shell;
 
-	while ((input = readline("expand_test$ ")))
+	shell.last_exit_status = 0;
+	shell.envp = NULL;
+	while (1)
 	{
-		expanded = expand_env(input, &shell);
-		printf("expanded: %s\n", expanded);
-		free(expanded);
+		input = readline("minishell$ ");
+		if (!input)
+			break ;
+		if (*input)
+			add_history(input);
+		process_input(input, &shell);
 		free(input);
 	}
 	return (0);
