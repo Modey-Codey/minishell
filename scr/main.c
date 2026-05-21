7034dfc6d1dfcbd7ef6a6815f184d75a5c4452c4
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-05-21 20:09:44 +0700

    unset.c

--- scr/main.c
+++ scr/main.c
@@ -1,36 +1,29 @@
 #include "minishell.h"
 
-static char **copy_env_main(char **envp)
-{
-    int     i;
-    char    **env;
-
-    i = 0;
-    while (envp[i])
-        i++;
-    env = malloc(sizeof(char *) * (i + 1));
-    if (!env)
-        return (NULL);
-    i = 0;
-    while (envp[i])
-    {
-        env[i] = ft_strdup(envp[i]);
-        i++;
-    }
-    env[i] = NULL;
-    return (env);
-}
-
 int main(int argc, char **argv, char **envp)
 {
-    char    **env;
+	t_shell shell;
+	int		i;
+	int		status;
 
-    (void)argc;
-    env = copy_env_main(envp);
-    if (!env)
-        return (1);
-    builtin_export(argv, &env); 
-    print_export(env);
-    free_arr(env);
+	(void)argc;
+	shell.envp = copy_env(envp);
+	printf("Befor unset:\n");
+	i = 0;
+	while (shell.envp[i])
+	{
+		printf("%s\n", shell.envp[i]);
+		i++;
+	}
+	status = builtin_unset(argv, &shell);
+	printf("\nAfter unset:\n");
+	i = 0;
+	while (shell.envp[i])
+	{
+		printf("%s\n", shell.envp[i]);
+		i++;
+	}
+	printf("\nExit Status: %d\n", status);
+	free_envp(shell.envp);
     return (0);
 }
