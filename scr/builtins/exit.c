3a22a5fa0c30196b208c8ba2974c22a33ab58afe
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-05-21 21:02:02 +0700

    exit.c

--- scr/builtins/exit.c
+++ scr/builtins/exit.c
@@ -1,9 +1,66 @@
 #include "minishell.h"
 
+static int	is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!is_digit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static long long	ft_atoll_exit(char *str)
+{
+	long long	result;
+	int			sign;
+	int			i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
 int	builtin_exit(char **argv, t_shell *shell)
 {
-	(void)argv;
-	(void)shell;
+	long long	exit_code;
+
 	printf("exit\n");
-	exit(0);
+	if (!argv[1])
+		exit(shell->last_exit_status);
+	if (!is_numeric(argv[1]))
+	{
+		printf("minishell: exit: %s: numeric argument required\n", argv[1]);
+		exit(255);
+	}
+	if (argv[2])
+	{
+		printf("minishell: exit: too many arguments\n");
+		shell->last_exit_status = 1;
+		return (1);
+	}
+	exit_code = ft_atoll_exit(argv[1]);
+	exit((unsigned char)exit_code);
 }
