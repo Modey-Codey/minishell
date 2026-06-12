4979e8d27be395045d193824af559b9d8224450b
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-06-12 23:26:38 +0700

    execute

---
+++ scr/execute/execute.c
@@ -0,0 +1,79 @@
+
+#include <minishell.h>
+
+
+void	execute_single_cmd(t_cmd *cmd, t_shell *shell)
+{
+	pid_t	pid;
+	int		status;
+	char *cmd_path;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		if (setup_redirection(cmd) != 0)
+			exit(1);
+		if (is_builtin(cmd->argv[0]))
+			exit(exec_builtin(cmd, shell, 1));
+		else
+		{
+			cmd_path = get_cmd_path(cmd->argv[0], shell->envp);
+			if (!cmd_path)
+			{
+				printf("minishell: \n%s: command not found\n", cmd->argv[0]);
+				exit(127);
+			}
+			execve(cmd_path, cmd->argv, shell->envp);
+			perror("minishell: execve");
+			free(cmd_path);
+			exit(126);
+		}
+	}
+	else
+	{
+		signal(SIGINT, SIG_IGN);
+		signal(SIGQUIT, SIG_IGN);
+		waitpid(pid, &status, 0);
+		init_signals();
+		if (WIFEXITED(status))
+			shell->last_exit_status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+		{
+			shell->last_exit_status = 128 + WTERMSIG(status);
+			if (WTERMSIG(status) == SIGQUIT)
+				write(1, "Quit (core dumped)\n", 19);
+			else if (WTERMSIG(status) == SIGINT)
+				write(1, "\n", 1);
+		}
+	}
+}
+
+
+void	execute(t_cmd *cmds, t_shell *shell)
+{
+	int	saved_stdin;
+	int	saved_stdout;
+
+	if (!cmds)
+		return ;
+	if (is_single_builtin(cmds))
+	{
+		saved_stdin = dup(STDIN_FILENO);
+		saved_stdout = dup(STDOUT_FILENO);
+		if (setup_redirection(cmds) == 0)
+			shell->last_exit_status = exec_builtin(cmds, shell, 1);
+		else
+			shell->last_exit_status = 1;
+		dup2(saved_stdin, STDIN_FILENO);
+		dup2(saved_stdout, STDOUT_FILENO);
+		close(saved_stdin);
+		close(saved_stdout);
+	}
+	else if (!cmds->next)
+		execute_single_cmd(cmds, shell);
+	else
+		execute_pipe(cmds, shell);
+
+}
