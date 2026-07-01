9d7d9a19e5420a6ee3bbe68e582236c7ec8d19b7
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-07-01 19:53:29 +0700

    final

--- utils/ft_strncmp.c
+++ utils/ft_strncmp.c
@@ -14,3 +14,48 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	if (!str || fd < 0)
+		return ;
+	write(fd, str, ft_strlen(str));
+}
+
+void	check_fd_error(t_cmd *cmd, int fd, char *val)
+{
+	if (fd < 0)
+	{
+		cmd->redir_error = 1;
+		cmd->redir_errno = errno;
+		cmd->redir_error_file = ft_strdup(val);
+	}
+	else
+		close(fd);
+}
+
+void	apply_file_redir(t_cmd *cmd, char *val, int type)
+{
+	int	fd;
+
+	if (type == APPEND)
+		fd = open(val, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (type == REDIR_OUT)
+		fd = open(val, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else
+		fd = open(val, O_RDONLY);
+	check_fd_error(cmd, fd, val);
+	if (type == REDIR_IN)
+	{
+		if (cmd->infile)
+			free(cmd->infile);
+		cmd->infile = val;
+	}
+	else
+	{
+		if (cmd->outfile)
+			free(cmd->outfile);
+		cmd->outfile = val;
+		cmd->append = (type == APPEND);
+	}
+}
