520e2ab18538e9415927e144ec179b60ed04787b
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-07-29 16:30:52 +0700

    fix leak

--- scr/builtins/pwd.c
+++ scr/builtins/pwd.c
@@ -1,14 +1,22 @@
 #include "minishell.h"
 
-int	builtin_pwd(void)
+int	builtin_pwd(t_shell *shell)
 {
 	char	cwd[4096];
+	char	*prev_pwd;
 
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
-		perror("pwd");
-		return (1);
+		printf("%s\n", cwd);
+		return (0);
 	}
-	printf("%s\n", cwd);
-	return (0);
+	prev_pwd = get_env_val(shell->envp, "PWD");
+	if (prev_pwd != NULL)
+	{
+		fprintf(stderr,
+			"pwd: error retrieving current directory: "
+			"getcwd: cannot access parent directories: "
+			"No such file or directory\n");
+	}
+	return (1);
 }
