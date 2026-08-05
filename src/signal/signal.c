6ae4ecd1bf8a5c0accbccab5714936c547dd8e17
Phawngam <136565948+Modey-Codey@users.noreply.github.com>
2026-08-05 22:43:35 +0700

    Done

---
+++ src/signal/signal.c
@@ -0,0 +1,34 @@
+#include "minishell.h"
+
+int	g_signal = 0;
+
+void	sigint_handler(int sig)
+{
+	g_signal = sig;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	heredoc_sigint_handler(int sig)
+{
+	g_signal = sig;
+	write(1, "\n", 1);
+	close(STDIN_FILENO);
+}
+
+void	init_signals(void)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	sa_int.sa_handler = sigint_handler;
+	sigaction(SIGINT, &sa_int, NULL);
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = SA_RESTART;
+	sa_quit.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
